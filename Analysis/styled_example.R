0de9d2d530b04251dadb3abbe202af86cf308db4
Haley Colgate Kottler <hcolgate@wisc.edu>
2025-11-06 08:38:35 -0600

    save sleep plots and update gitignore

--- Analysis/styled_example.R
+++ Analysis/styled_example.R
@@ -16,7 +16,9 @@ for (colname in colnames(df)[colnames(df) %in% c("genus", "vore", "order", "cons
   all.plots[[length(all.plots) + 1]] <- ggplot(dat.summary) +
     geom_col(aes(x = .data[[colname]], y = sleep_total)) +
     ylab("Avg Total Sleep (hrs)") +
-    theme(axis.text.x = element_text(angle = 30, hjust = 1, vjust = 1))
+    theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.25))
 }
 
 wrap_plots(all.plots, ncol = 2, widths = c(2, 1))
+ggsave(file.path("Figures", "sleep_plots.png"),
+       width = 10, height = 5, units = "in")
